f2747cfe97d686c79ee58cd29765ef4a5c75c86d
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2025-04-03 13:21:34 +0300

    update  file

---
+++ db/models/contact.js
@@ -0,0 +1,21 @@
+import { DataTypes } from "sequelize";
+
+import sequelize from "../Sequelize.js";
+
+const Contact = sequelize.define("Contact", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+// Contact.sync();
+export default Contact;
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -1,54 +1,21 @@
-import fs from "fs/promises";
-import path from "path";
-import { nanoid } from "nanoid";
-
-// Шлях до файлу contacts.json
-const contactsPath = path.resolve("db", "contacts.json");
+import Contact from "../db/models/contact.js";
 
 // Функція для зчитування всіх контактів
-export async function listContacts() {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  const contacts = JSON.parse(data);
-  return contacts;
-}
-
-// Функція для отримання контакту за ID
-export async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const contact = contacts.find((contact) => contact.id === contactId);
-  return contact || null;
-}
-
-// Функція для видалення контакту за ID
-export async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  const [removedContact] = contacts.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return removedContact;
-}
+export const listContacts = () => Contact.findAll();
 
 // Функція для додавання нового контакту
-export async function addContact(name, email, phone) {
-  const newContact = { id: nanoid(), name, email, phone };
-  const contacts = await listContacts();
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return newContact;
-}
+export const addContact = (name, email, phone) =>
+  Contact.create({ name, email, phone });
 
-// Функція для оновлення контакту за ID
-export async function updateContact(id, updatedData) {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((contact) => contact.id === id);
-  if (index === -1) return null;
+// Функція для отримання контакту за ID
+export const getContactById = (id) => Contact.findByPk(id);
 
-  const updatedContact = { ...contacts[index], ...updatedData };
+export const updateContact = async (id, updatedContact) => {
+  const contact = await getContactById(id);
+  if (!contact) return null;
 
-  contacts[index] = updatedContact;
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  return updatedContact;
-}
+  return contact.update(updatedContact);
+};
+
+// Функція для видалення контакту за ID
+export const removeContact = (id) => Contact.destroy({ where: { id } });
